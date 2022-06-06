a2404290b989311fe5c30bc69ab7d2c0fb5f9a2a
jvfe <jvfecav@gmail.com>
2022-06-06 18:30:01 -0300

    fix: Change file in last task

--- wf/__init__.py
+++ wf/__init__.py
@@ -5,16 +5,16 @@ Assemble and sort some COVID reads...
 import subprocess
 from pathlib import Path
 
-from latch import medium_task, small_task, workflow
+from latch import small_task, workflow
 from latch.types import LatchFile
 
-# Kaiju index path
-KAIJU_IDX = "/root/reference/"
 
-
-@medium_task
+@small_task
 def taxonomy_classification_task(read1: LatchFile, read2: LatchFile) -> LatchFile:
-    global KAIJU_IDX
+
+    # Kaiju index path
+    KAIJU_IDX = "/root/reference/"
+
     # A reference to our output.
     output_name = "virus_kaiju.out"
     kaiju_out = Path(output_name).resolve()
@@ -42,7 +42,9 @@ def taxonomy_classification_task(read1: LatchFile, read2: LatchFile) -> LatchFil
 
 @small_task
 def kaiju2krona_task(kaiju_out: LatchFile) -> LatchFile:
-    global KAIJU_IDX
+    # Kaiju index path
+    KAIJU_IDX = "/root/reference/"
+
     output_name = "virus_kaiju2krona.out"
     krona_txt = Path(output_name).resolve()
 
@@ -68,7 +70,7 @@ def plot_krona_task(krona_txt: LatchFile) -> LatchFile:
     output_name = "virus_krona.html"
     krona_html = Path(output_name).resolve()
 
-    _kaiju2krona_cmd = ["ktImportText", "-o", str(krona_txt), krona_txt.local_path]
+    _kaiju2krona_cmd = ["ktImportText", "-o", str(krona_html), krona_txt.local_path]
 
     subprocess.run(_kaiju2krona_cmd)
 
